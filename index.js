9e9c2692158083a40818024f16b8b256ea210831
Talha <mdtalha2008@gmail.com>
2022-11-11 03:16:49 +0600

    port running

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const app=express();
 const port=process.env.PORT || 5000;
 
 //middle wares
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('running task');
+})
+
+app.listen(port,()=>{
+    console.log(`task running on ${port}`);
+})
